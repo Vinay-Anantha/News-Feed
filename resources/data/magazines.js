1e3a2a228b3126f487953f32db19d006bbeaea22
Vinay Anantha <vinayanantha096@gmail.com>
2021-02-01 00:51:19 +0530

    Add files via upload

---
+++ resources/data/magazines.js
@@ -0,0 +1,13 @@
+//magazines = [
+//   "https://flipboard.com/@electioncentral/first-100-days-qhnfhr05z.rss",
+//   "https://flipboard.com/@flipboard/10-for-today-k6ln1khuz.rss",
+//   "https://flipboard.com/@health/coronavirus-the-hub-mbc9fs4t93llu0mb.rss"
+// ];
+
+
+magazines = [
+  "https://storage.googleapis.com/fullstack-assessment/coronavirus.rss",
+  "https://storage.googleapis.com/fullstack-assessment/india-tech.rss",
+  "https://storage.googleapis.com/fullstack-assessment/sports-star.rss"
+]
+
---
+++ resources/js/index.js
@@ -0,0 +1,62 @@
+const api2Json = "https://api.rss2json.com/v1/api.json?rss_url=";
+
+async function init() {
+  for (let index = 0; index < magazines.length; index++) {
+    const feeds = await fetchFeed(magazines[index]);
+    addCityToDOM(feeds["items"], index);
+  }
+}
+
+async function fetchFeed(url) {
+  try {
+    const response = await fetch(api2Json + url);
+    if (response.status >= 200 && response.status <= 299) {
+      // console.log(response);
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    } else {
+      throw Error(response.statusText);
+    }
+  } catch (error) {
+    return null;
+  }
+}
+
+function addCityToDOM(objArr, index) {
+  const temp = [2, 3, 4];
+  const renderHook = document.querySelector(
+    `#accordion > div:nth-child(${index + temp[index]}) > div > div`
+  );
+  objArr.forEach((obj, index) => {
+    renderHook.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="carousel-item">
+      <div class="card">
+      <a href="${obj.link}" target="_blank">
+      <img src="${obj.enclosure.link}"  class="card-img-top" alt="Image not available"></img>
+      <div class="card-body">
+          <h5 class="card-title">${obj.title}</h5>
+          <span class="card-subtitle mb-2 text-muted">
+          ${obj.author}
+          </span>
+          <span class="card-subtitle mb-2 text-muted">
+          ${obj.pubDate}
+          </span>
+          <p class="card-text">
+          ${obj.description}
+          </p>
+        </div>
+        </a>
+      </div>
+    </div>`
+    );
+  });
+  renderHook.querySelector("div:nth-child(1)").classList.add("active");
+}
+
+$("#accordion").on("hide.bs.collapse show.bs.collapse", (e) => {
+  $(e.target).prev().find("i").toggleClass("fa-angle-up fa-angle-down");
+});
+
+init();
